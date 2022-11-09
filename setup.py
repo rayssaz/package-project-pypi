b20938d9d77fcbc0e7236b9f356e971892394b47
rayssaz <rayssacristinne23@gmail.com>
2022-11-09 19:42:00 -0300

    atualizando o setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="bubble",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Rayssa Pereira",
+    author_email="rayssacristinne23@gmail.com",
+    description="O pacote contém o algoritmo de ordenação Bubble Sort, o qual pode ser aplicado em arrays e listas dinâmicas.. ",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/rayssaz/package-project-pypi"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
